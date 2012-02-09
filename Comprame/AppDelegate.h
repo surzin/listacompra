814a4df088cf5bd3573de17a99b18a931ca4eddf
Antonio Gil Sanchez <agil@MbpAgil.local>
2012-02-09 18:38:17 +0100

    Initial Commit

---
+++ Comprame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Comprame
+//
+//  Created by Antonio Gil Sánchez on 09/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Comprame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Comprame
+//
+//  Created by Antonio Gil Sánchez on 09/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
