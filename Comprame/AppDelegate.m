c8e6079f434e2af33d7e853158853062d2d8ba20
henche <carlos.henche@gmail.com>
2012-02-18 12:56:07 +0100

    Comentario Carlos

--- Comprame/AppDelegate.m
+++ Comprame/AppDelegate.m
@@ -17,6 +17,7 @@
     // Override point for customization after application launch.
     return YES;
     //agil agil
+    //cperez cperez
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
